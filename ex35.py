bdc4850aabc810197015ace9f379d0ae68c4b873
Rex <r030515495@gmail.com>
2011-11-26 15:28:40 +0800

    test2 Signed-off-by: Rex <r030515495@gmail.com>

--- ex35.py
+++ ex35.py
@@ -2,3 +2,23 @@
 #!/usr/bin/env python
 
 
+from sys import exit
+
+def gold_room():
+    print "This room is full of gold "
+    
+    next = raw_input(">")
+    if "0" in next or "1" in next:
+        how_much = int(next)
+    else:
+        dead("Man,")
+        
+    if how_much < 50 :
+        print "Nice"
+        exit(0)
+    else :
+        dead("yout greedy bastarf")
+        
+        
+def bear_room():
+    print "dd"
